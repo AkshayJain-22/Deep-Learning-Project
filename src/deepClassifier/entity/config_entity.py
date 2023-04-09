92e51f92efcfe7aabbb2a0a6e627a1f620d82afe
Akshay Jain <akshay22.11.1995@gmail.com>
2023-04-09 17:23:01 +0530

    Stage02 Added

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -7,3 +7,14 @@ class DataIngestionConfig:      #this will also work as a named tuple but is a b
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True) 
+class PrepareBaseModelConfig:      #this will also work as a named tuple but is a better way to implement the same concept
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_include_top: bool
+    params_learning_rate: float
+    params_weights: str
+    params_classes:int
